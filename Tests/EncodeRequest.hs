dc8d2f08b047a0d692aa5bd8815161620d150c09
Vincent Hanquez <vhanquez@pivotmail.com>
2014-11-26 10:59:41 +0000

    add copyright headers

--- Tests/EncodeRequest.hs
+++ Tests/EncodeRequest.hs
@@ -1,11 +1,20 @@
--- |
--- Module      : Tests.EncodeRequest
--- License     : BSD-Style
--- Copyright   : Copyright © 2014 AlephCloud Systems, Inc.
+-- Copyright (c) 2013-2014 PivotCloud, Inc.
 --
--- Maintainer  : Nicolas DI PRIMA <ndiprima@alephcloud.com>
--- Stability   : experimental
--- Portability : unknown
+-- EncodeRequest
+--
+-- Please feel free to contact us at licensing@pivotmail.com with any
+-- contributions, additions, or other feedback; we would love to hear from
+-- you.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License"); you may
+-- not use this file except in compliance with the License. You may obtain a
+-- copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+-- WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+-- License for the specific language governing permissions and limitations
+-- under the License.
 --
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE OverloadedStrings #-}
